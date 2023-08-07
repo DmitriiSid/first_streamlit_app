7e55be3250e888a62f4e8fc62e730c70e4ad9138
Dmitrii Sidorenko <100510357+DmitriiSid@users.noreply.github.com>
2023-08-07 15:33:21 +0200

    Update streamlit_app.py
    Change of the output

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,3 +26,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+#Display the table on the page.
+streamlit.dataframe(fruityvice_normalized)
